2bd37550a0e2af6c9a5763385cfd05eb0f9ab66f
Naveen_KR <newman81288@yahoo.in>
2020-10-04 16:53:38 +0530

    Program for preprocessing the signal before transmitting to the server end.

---
+++ preprocess.py
@@ -0,0 +1,14 @@
+
+
+
+
+#################..............define the function custum_filter..........######
+
+def custum_filter(rawdata, fs):
+    '''The main function which does filtering of the raw data.
+    Inputs are raw data chunk and sampling frequency and outpus processed data'''
+
+    processed_data = rawdata
+
+    return (processed_data)
+    
