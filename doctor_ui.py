48cb3e170f5ea976e4a00ddb3c8e939ee3300688
Naveen_KR <newman81288@yahoo.in>
2020-11-17 19:24:36 +0530

    Added a plotting function and recording function at the doctor end.

--- doctor_ui.py
+++ doctor_ui.py
@@ -2,7 +2,9 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import socket
 import threading
 import pyaudio
-#from doctor import Client
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -11,7 +13,7 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(120, 50, 541, 20))
+        self.label.setGeometry(QtCore.QRect(100, 50, 700, 20))
         font = QtGui.QFont()
         font.setPointSize(12)
         font.setBold(True)
@@ -19,7 +21,7 @@ class Ui_MainWindow(object):
         self.label.setFont(font)
         self.label.setObjectName("label")
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(120, 180, 41, 16))
+        self.label_3.setGeometry(QtCore.QRect(120, 180, 50, 16))
         font = QtGui.QFont()
         font.setPointSize(10)
         self.label_3.setFont(font)
@@ -37,7 +39,7 @@ class Ui_MainWindow(object):
         self.label_5.setFont(font)
         self.label_5.setObjectName("label_5")
         self.label_6 = QtWidgets.QLabel(self.centralwidget)
-        self.label_6.setGeometry(QtCore.QRect(120, 10, 61, 16))
+        self.label_6.setGeometry(QtCore.QRect(120, 10, 75, 16))
         font = QtGui.QFont()
         font.setPointSize(10)
         self.label_6.setFont(font)
@@ -61,7 +63,7 @@ class Ui_MainWindow(object):
         self.port.setGeometry(QtCore.QRect(450, 90, 101, 20))
         self.port.setObjectName("port")
         self.connect_pat = QtWidgets.QPushButton(self.centralwidget)
-        self.connect_pat.setGeometry(QtCore.QRect(290, 130, 141, 23))
+        self.connect_pat.setGeometry(QtCore.QRect(250, 130, 180, 23))
         font = QtGui.QFont()
         font.setPointSize(10)
         font.setBold(True)
@@ -69,10 +71,10 @@ class Ui_MainWindow(object):
         self.connect_pat.setFont(font)
         self.connect_pat.setObjectName("connect_pat")
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(190, 10, 211, 20))
+        self.lineEdit.setGeometry(QtCore.QRect(200, 10, 211, 20))
         self.lineEdit.setObjectName("lineEdit")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(170, 182, 47, 13))
+        self.label_2.setGeometry(QtCore.QRect(180, 182, 47, 13))
         self.label_2.setObjectName("label_2")
         self.label_9 = QtWidgets.QLabel(self.centralwidget)
         self.label_9.setGeometry(QtCore.QRect(390, 182, 47, 13))
@@ -81,7 +83,7 @@ class Ui_MainWindow(object):
         self.label_10.setGeometry(QtCore.QRect(530, 182, 47, 13))
         self.label_10.setObjectName("label_10")
         self.label_11 = QtWidgets.QLabel(self.centralwidget)
-        self.label_11.setGeometry(QtCore.QRect(240, 210, 131, 20))
+        self.label_11.setGeometry(QtCore.QRect(200, 210, 131, 20))
         font = QtGui.QFont()
         font.setPointSize(10)
         font.setBold(True)
@@ -99,7 +101,7 @@ class Ui_MainWindow(object):
         self.conn_status.setGeometry(QtCore.QRect(290, 155, 141, 20))
         self.conn_status.setObjectName("conn_status")
         self.record = QtWidgets.QPushButton(self.centralwidget)
-        self.record.setGeometry(QtCore.QRect(300, 260, 121, 23))
+        self.record.setGeometry(QtCore.QRect(250, 260, 150, 23))
         font = QtGui.QFont()
         font.setPointSize(10)
         font.setBold(True)
@@ -111,17 +113,26 @@ class Ui_MainWindow(object):
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
+        self.out_image = QtWidgets.QLabel(self.centralwidget)
+        self.out_image.setGeometry(QtCore.QRect(50, 300, 620, 200))
+        self.out_image.setText("")
+        self.out_image.setScaledContents(True)
+        self.out_image.setObjectName("out_image")
+
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
 
         self.connect_pat.clicked.connect(lambda: self.clicked())
+        self.record.clicked.connect(lambda: self.clicked_rec())
+
+        self.rcvd_data = []
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Telesthoscope-Doctor_UI"))
         self.label.setText(_translate("MainWindow", "Please enter the IP address and port to connect to your doctor"))
-        self.label_3.setText(_translate("MainWindow", "Name: "))
+        self.label_3.setText(_translate("MainWindow", "Name:"))
         self.label_4.setText(_translate("MainWindow", "Age:"))
         self.label_5.setText(_translate("MainWindow", "Sex:"))
         self.label_6.setText(_translate("MainWindow", "Patient ID:"))
@@ -136,7 +147,7 @@ class Ui_MainWindow(object):
         self.comboBox.setItemText(1, _translate("MainWindow", "Aortic"))
         self.comboBox.setItemText(2, _translate("MainWindow", "Pulmonary"))
         self.comboBox.setItemText(3, _translate("MainWindow", "Tricuspid"))
-        self.conn_status.setText(_translate("MainWindow", "          (Not connected)"))
+        self.conn_status.setText(_translate("MainWindow", "      (Not connected)"))
         self.record.setText(_translate("MainWindow", "Start Recording"))
 
     def clicked(self):
@@ -153,11 +164,11 @@ class Ui_MainWindow(object):
                 self.port_no = int(self.port.text())
                 self.s.connect((self.ip, self.port_no))
                 self.start()
-                self.conn_status.setText("           (Connected)")
+                self.conn_status.setText("          (Connected)")
                 self.connect_pat.setText("  Disconnect")
                 
             except:
-                self.conn_status.setText("       (Incorrect Details)")
+                self.conn_status.setText("      (Incorrect Details)")
 
         else:
             self.s.close()
@@ -181,11 +192,29 @@ class Ui_MainWindow(object):
         while True:
             try:
                 data = self.s.recv(2096)
-                #print(sys.getsizeof(data))
                 self.playing_stream.write(data)
+                if self.record.text()== "Stop Recording":
+                    numpy_data = np.frombuffer(data, dtype = np.int16)
+                    self.rcvd_data = np.append(self.rcvd_data, numpy_data)
+                #print(sys.getsizeof(data))
+                    
             except:
                 pass
-       
+
+    def clicked_rec(self):
+        if self.record.text() == "Start Recording":
+            self.record.setText("Stop Recording")
+        else:
+            self.record.setText("Start Recording")
+            if len(self.rcvd_data)!=0:
+                plt.plot(self.rcvd_data)
+                plt.savefig("test.svg",format="svg",bbox_inches="tight")
+                plt.close()
+                self.rcvd_data = []
+                try:
+                    self.out_image.setPixmap(QtGui.QPixmap("test.svg"))
+                except:
+                    pass
         
 if __name__ == "__main__":
     import sys
